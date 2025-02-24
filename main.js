085bde7de8b1574b2a004c3a8e2807fb41d153ef
LindaTancini <lindatancini95@gmail.com>
2025-02-24 15:17:37 +0100

    feat: add 20% discountprice

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// Dati delle variabili
-
-const kmPrice = 0.21;
-const sum = kmPrice * km;
-let percentage = 0;
-let finalPrice = 0;
-let discountPrice = 0;
-
 // km da percorrere
 
 const km = parseInt(prompt("Quanti kilometri devi percorrere?"));
@@ -16,4 +8,17 @@ console.log(km);
 const userAge = parseInt(prompt("Inserisci la tua età"));
 console.log(userAge);
 
+// Dati delle variabili
+
+const kmPrice = 0.21;
+const sum = kmPrice * km;
+let percentage = 0;
+let finalPrice = 0;
+let discountPrice = 0;
+
 // Sconto 20% per minorenni < 18
+if (userAge < 18) {
+  percentage = 20;
+  discountPrice = (sum / 100) * percentage;
+  console.log(discountPrice);
+}

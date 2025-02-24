c9a5e2df22e85b09f6f646c7d3fcabad639bc53d
LindaTancini <lindatancini95@gmail.com>
2025-02-24 14:53:51 +0100

    feat: added kilometers to travel

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const kmPrice = 0.21;
 let percentage = 0;
 let finalPrice = 0;
 let discountPrice = 0;
+
+// km da percorrere
+
+const km = parseInt(prompt("Quanti kilometri devi percorrere?"));
+console.log(km);

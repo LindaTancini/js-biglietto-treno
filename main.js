96e1b85d8c6a720cb6a48be84ea2c5d20dbee003
LindaTancini <lindatancini95@gmail.com>
2025-02-24 15:59:15 +0100

    feat:  add finalprice

--- main.js
+++ main.js
@@ -21,9 +21,14 @@ if (userAge < 18) {
   percentage = 20;
   discountPrice = (sum / 100) * percentage;
   console.log(discountPrice);
-  // Sconto 40% over 65
+  // Sconto 40% over >65
 } else if (userAge >= 65) {
   percentage = 40;
   discountPrice = (sum / 100) * percentage;
   console.log(discountPrice);
 }
+
+// Prezzo finale con massimo due decimali
+
+finalPrice = (sum - discountPrice).toFixed(2);
+console.log(finalPrice);
